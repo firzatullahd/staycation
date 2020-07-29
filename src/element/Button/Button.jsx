b5f3292d1d21c5573a027faaba906e50f97075c3
Firza17 <firzatullahdwikoramadhan@gmail.com>
2020-07-29 14:44:21 +0700

    new button element

---
+++ src/element/Button/Button.jsx
@@ -0,0 +1,84 @@
+import React, { Fragment } from "react";
+import { Link } from "react-router-dom";
+import propTypes from "prop-types";
+
+export default function Button(props) {
+  const className = [props.className];
+  if (props.isPrimary) className.push("btn-primary");
+  if (props.isLarge) className.push("btn-lg");
+  if (props.isSmall) className.push("btn-sm");
+  if (props.isBlock) className.push("btn-block");
+  if (props.hasShadow) className.push("btn-shadow");
+
+  const onClick = () => {
+    if (props.onClick) props.onClick;
+  };
+  if (props.isDisabled || isLoading) {
+    if (props.isDisabled) className.push("disabled");
+    return (
+      <span className={className.join(" ")} style={props.style}>
+        {props.isLoading ? (
+          <Fragment>
+            <span className="spinner-border spinner-border-sm mx-5"></span>
+            <span className="sr-only">Loading...</span>
+          </Fragment>
+        ) : (
+          props.children
+        )}
+      </span>
+    );
+  }
+
+  if (props.type === "link") {
+    if (props.isExternal) {
+      return (
+        <a
+          href={props.href}
+          className={className.join(" ")}
+          style={props.style}
+          target={props.target === "blank" ? "blank" : undefined}
+          rel={props.target === "noopener noreferrer" ? "blank" : undefined}
+        >
+          {props.children}
+        </a>
+      );
+    } else
+      return (
+        <Link
+          className={className.join(" ")}
+          to={props.href}
+          style={props.style}
+          onClick={onClick}
+        >
+          {props.children}
+        </Link>
+      );
+  }
+
+  return (
+    <Button
+      className={className.join(" ")}
+      to={props.href}
+      style={props.style}
+      onClick={onClick}
+    >
+      {props.children}
+    </Button>
+  );
+}
+
+Button.propTypes = {
+  type: propTypes.oneOf(["button", "link"]),
+  onClick: propTypes.func,
+  target: propTypes.string,
+  href: propTypes.string,
+  className: propTypes.string,
+  isPrimary: propTypes.bool,
+  isDisabled: propTypes.bool,
+  isLoading: propTypes.bool,
+  isSmall: propTypes.bool,
+  isLarge: propTypes.bool,
+  isBlock: propTypes.bool,
+  isExternal: propTypes.bool,
+  hasShadow: propTypes.bool,
+};
---
+++ src/element/Button/Button.test.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import { render } from '@testing-library/react'
+// import { BrowserRouter as Router } from 'react-router-dom'
+
+import Button from './Button'
+
+test("Shouldn't be able to click button if isDisabled is present", () => {
+    const { container } = render(<Button isDisabled></Button>)
+
+    expect(container.querySelector('span.disabled')).toBeInTheDocument()
+})
+
+
+test("Should render loading/spinner", () => {
+    const { container, getByText } = render(<Button isLoading></Button>)
+
+    expect(getByText(/loading/i)).toBeInTheDocument()
+
+    expect(container.querySelector("span")).toBeInTheDocument()
+})
