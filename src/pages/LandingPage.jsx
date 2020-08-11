411e2d739786b4017bcc6951c843f8595fa37a76
Firza17 <firzatullahdwikoramadhan@gmail.com>
2020-08-12 05:52:55 +0700

    add img and animation to mostpicked component

--- src/pages/LandingPage.jsx
+++ src/pages/LandingPage.jsx
@@ -3,17 +3,24 @@ import Header from "../component/Header";
 import Hero from "../component/Hero";
 import MostPicked from "../component/MostPicked";
 
-// import landingPageJson from "../json/landingPage.json";
+import landingPageData from "../json/landingPage.json";
 
 export default class LandingPage extends Component {
+  constructor(props) {
+    super(props);
+    this.refMostPicked = React.createRef();
+  }
   render() {
-    const landingPageData = require("../json/landingPage.json");
+    // const landingPageData = require("../json/landingPage.json");
     console.log(landingPageData);
     return (
       <Fragment>
         <Header {...this.props}></Header>
-        <Hero data={landingPageData.hero} />
-        <MostPicked data={landingPageData.mostPicked} />
+        <Hero refMostPicked={this.refMostPicked} data={landingPageData.hero} />
+        <MostPicked
+          refMostPicked={this.refMostPicked}
+          data={landingPageData.mostPicked}
+        />
       </Fragment>
     );
   }
