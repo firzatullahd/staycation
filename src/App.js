3a53e3677f8437bd2fe612f38bbf65ba15c0ed57
Firza17 <firzatullahdwikoramadhan@gmail.com>
2020-08-20 15:29:55 +0700

    create new component inputDate

--- src/App.js
+++ src/App.js
@@ -2,15 +2,15 @@ import React from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom'
 import LandingPage from './pages/LandingPage'
 import './assets/scss/style.scss';
-import InputNumber from './element/Form/InputNumber/InputNumber'
+// import InputNumber from './element/Form/InputNumber/InputNumber'
 
 function App() {
   return (
     <div className="App">
-      {/* <Router>
+      <Router>
         <Route path="/" component={LandingPage}></Route>
-      </Router> */}
-      <InputNumber />
+      </Router>
+      {/* <InputNumber /> */}
     </div>
   );
 }
---
+++ src/element/Form/InputDate/InputDate.jsx
@@ -0,0 +1,89 @@
+import React, { useState, useRef, useEffect } from "react";
+import propTypes from "prop-types";
+
+import { DateRange } from "react-date-range";
+
+import "./InputDate.scss";
+import "react-date-range/dist/styles.css";
+import "react-date-range/dist/theme/default.css";
+
+import formatDate from "../../../utils/formatDate";
+import iconCalendar from "../../../assets/icons/ic_calendar.svg";
+
+export default function Date() {
+  const { value, placeholder, name } = props;
+  const [isShown, setisShown] = useState(false);
+
+  const datePickerChange = (value) => {
+    const target = {
+      target: {
+        value: value.selection,
+        name: name,
+      },
+    };
+    props.onChange(target);
+  };
+
+  useEffect(() => {
+    document.addEventListener("mousedown", handleClickOutside);
+    return () => {
+      document.removeEventListener("mousedown", handleClickOutside);
+    };
+  });
+
+  const refDate = useRef(null);
+  const handleClickOutside = (e) => {
+    if (refDate && refDate.current.contains(e.target)) {
+      setisShown(false);
+    }
+  };
+
+  const check = (focus) => {
+    focus.indexOf(1) < 0 && setisShown(false);
+  };
+
+  const diplayDate = `${value.startDate ? formatDate(value.startDate) : ""} ${
+    value.endDate ? "-" + formatDate(value.endDate) : ""
+  } `;
+  return (
+    <div
+      ref={refDate}
+      className={["input-date mb-3", props.outerClassName].join(" ")}
+    >
+      <div className="input-group">
+        <div className="input-group-prepend bg-gray-900">
+          <span className="input-group-text">
+            <img src={iconCalendar} alt="icon calendar" />
+          </span>
+        </div>
+        <input
+          readOnly
+          type="text"
+          className="form-control"
+          value={displayDate}
+          placeholder={placeholder}
+          onClick={() => setIsShown(!isShown)}
+        />
+
+        {isShown && (
+          <div className="date-range-wrapper">
+            <DateRange
+              editableDateInputs={true}
+              onChange={datePickerChange}
+              moveRangeOnFirstSelection={false}
+              onRangeFocusChange={check}
+              ranges={[value]}
+            />
+          </div>
+        )}
+      </div>
+    </div>
+  );
+}
+
+Date.propTypes = {
+  value: propTypes.object,
+  onChange: propTypes.func,
+  placeholder: propTypes.string,
+  outerClassName: propTypes.string,
+};
